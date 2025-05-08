f3b7c0ce7d254bb5c15fc1cc0ce2668fce070a72
SERVER <mehdifurkhan00@gmail.com>
2025-05-09 04:16:14 +0530

    Create reference.py

---
+++ blood-report-check/reference.py
@@ -0,0 +1,15 @@
+REFERENCE = {
+    "hemoglobin": {
+        "male":    {"range": (13.2, 16.6), "unit": "g/dl"},
+        "female":  {"range": (11.6, 15.0), "unit": "g/dl"}
+    },
+    "mch": {
+        "male":    {"range": (27, 33), "unit": "pg"},
+        "female":  {"range": (27, 33), "unit": "pg"}
+    },
+    "mchc": {
+        "male":    {"range": (33, 36), "unit": "g/dl"},
+        "female":  {"range": (33, 36), "unit": "g/dl"}
+    },
+    # Add all other tests in this format as needed...
+}
