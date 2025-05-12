c49aa9e57895b90dbde18d51bbf457fe9473b206
SERVER <mehdifurkhan00@gmail.com>
2025-05-13 01:44:32 +0530

    Update models.py

--- patient-registration/app/core/models.py
+++ patient-registration/app/core/models.py
@@ -8,7 +8,6 @@ class Patient(db.Model):
     first_name    = db.Column(db.String(50), nullable=False)
     last_name     = db.Column(db.String(50), nullable=False)
     date_of_birth = db.Column(db.Date, nullable=False)
-    age           = db.Column(db.Integer, nullable=False)
     sex           = db.Column(db.Enum('Male','Female','Other', name='sex_enum'), nullable=False)
     email         = db.Column(db.String(120), nullable=False, unique=True)
     phone         = db.Column(db.String(20), nullable=False)
