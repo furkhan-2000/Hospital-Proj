425431b2f23718bcf7122d47e3335ca9c8a7ad2a
SERVER <mehdifurkhan00@gmail.com>
2025-05-23 18:45:16 +0530

    Delete patient-registration directory

--- patient-registration/app/core/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Define blueprint
-bp = Blueprint("core", __name__, url_prefix="/api")
-
-# Register routes (must be after bp is defined)
-from . import routes
--- patient-registration/app/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-from flask_mail import Mail
-
-db = SQLAlchemy()
-limiter = Limiter(key_func=get_remote_address, default_limits=["200/day", "50/hour"])
-mail = Mail()
